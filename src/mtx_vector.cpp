bf3e126e2292d6c79e141eefd0d883d02bb83226
Hadi Sinaee <sinaee.hadi@gmail.com>
2019-01-09 23:39:58 +0330

    add isEmpty, overload [] operator

--- src/mtx_vector.cpp
+++ src/mtx_vector.cpp
@@ -1,39 +1,34 @@
 #include "mtx_vector.h"
-#include <fstream>
 #include <algorithm>
+#include <fstream>
 
-template <class T>
-int MtxVector<T>::getM() {
-  return this->M;
-}
+template <class T> int MtxVector<T>::getM() { return this->M; }
 
-template <class T>
-void MtxVector<T>::setDataAt(int idx, T value) {
+template <class T> void MtxVector<T>::setDataAt(int idx, T value) {
   this->data[idx] = value;
 }
 
-template <class T>
-T MtxVector<T>::getDataAt(int idx) {
+template <class T> T MtxVector<T>::getDataAt(int idx) {
   return this->data[idx];
 }
 
-template <class T>
-void MtxVector<T>::readMtxData(std::string filePath) {
+template <class T> void MtxVector<T>::readMtxData(std::string filePath) {
   int c;
   // open the file
   std::ifstream fin(filePath);
 
   // ignore headers and comments
-  while (fin.peek() == '%') fin.ignore(2048, '\n');
+  while (fin.peek() == '%')
+    fin.ignore(2048, '\n');
 
-  // read in dimensions
+  // read in dimension of vector
   fin >> this->M >> c;
 
   // create the vector values
-  this->data = new long double[M];
+  this->data = new T[M];
 
   // read the the vector data
-  long double input = 0;
+  T input = 0;
   int idx = 0;
   while (fin >> input) {
     this->setDataAt(idx, input);
@@ -41,4 +36,10 @@ void MtxVector<T>::readMtxData(std::string filePath) {
   }
 
   fin.close();
+}
+
+template <class T> bool MtxVector<T>::isEmpty() { return !this->data; }
+
+template <class T> T MtxVector<T>::operator[](int idx) {
+  return this->data[idx];
 }
--- src/mtx_vector.h
+++ src/mtx_vector.h
@@ -1,16 +1,20 @@
-// TODO: add a template for value of vector/matrix
-template <class T>
-class MtxVector {
+template <class T> class MtxVector {
 private:
-  int M; // number of rows
-  T* data; // matrix data
+  int M;   // number of rows
+  T *data; // matrix data
 
 public:
+  // read file from .mtx
+  void readMtxData(std::string);
+  // checks for emptiness of the vector
+  bool isEmpty();
+  // overloading [] oberator for accessing data
+  T operator[](int);
+
   // getter(s)
   int getM();
   T getDataAt(int);
 
   // setter(s)
   void setDataAt(int, T);
-  void readMtxData(std::string);
 };
